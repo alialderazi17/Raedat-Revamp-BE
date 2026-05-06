d49224c19e92fe0482925929c3e30660581f7a5d
Ali AlDerazi <ali.alderazi0@gmail.com>
2026-05-06 14:50:24 +0300

    database connection setup

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 const express = require('express')
 const cors = require('cors')
 const morgan = require('morgan')
-require('dotenv').config()
 
 const PORT = process.env.PORT || 3000
 
-// const db = require('./db')
+const db = require('./db')
 const app = express()
 
 app.use(cors())
