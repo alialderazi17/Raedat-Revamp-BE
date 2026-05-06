81004ff0ea10096999f44adecd74c5c2eea77f3d
Ali AlDerazi <ali.alderazi0@gmail.com>
2026-05-06 14:43:12 +0300

    server setup

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const cors = require('cors')
+const morgan = require('morgan')
+require('dotenv').config()
+
+const PORT = process.env.PORT || 3000
+
+// const db = require('./db')
+const app = express()
+
+app.use(cors())
+app.use(morgan('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.get('/', (req, res) => {
+  res.send("Server's Running!")
+})
+
+app.listen(PORT, () => {
+  console.log(`Express server running on port ${PORT}`)
+})
