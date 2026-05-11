342c4a64c1946b31154c183448975a71eb2ab0cb
Ali AlDerazi <ali.alderazi0@gmail.com>
2026-05-11 16:06:05 +0300

    not sure what i changed ill check the log

--- server.js
+++ server.js
@@ -4,15 +4,12 @@ const morgan = require('morgan')
 
 const PORT = process.env.PORT || 3000
 
-const authRouter = require('./routes/authRouter')
-const communityRouter = require('./routes/communityRouter')
-const newsletterRouter = require('./routes/newsletterRouter')
 const db = require('./db')
-const authRouter = require('./routes/authRouter')
-const eventRouter = require('./routes/eventRouter')
 
+const authRouter = require('./routes/authRouter')
 const communityRouter = require('./routes/communityRouter')
-const db = require('./db')
+const eventRouter = require('./routes/eventRouter')
+const newsletterRouter = require('./routes/newsletterRouter')
 const app = express()
 
 app.use(cors())
@@ -23,9 +20,8 @@ app.use(express.urlencoded({ extended: false }))
 app.use('/auth', authRouter)
 app.use('/communities', communityRouter)
 app.use('/newsletter', newsletterRouter)
-app.use('/auth', authRouter)
+
 app.use('/event', eventRouter)
-app.use('/communities', communityRouter)
 
 app.get('/', (req, res) => {
   res.send("Server's Running!")
