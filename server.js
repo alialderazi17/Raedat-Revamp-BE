3aeccd34194683dbd50c1c69e82cfb67c484acdb
Ali AlDerazi <ali.alderazi0@gmail.com>
2026-05-12 09:42:00 +0300

    Clean up duplicate router imports and usages
    Removed duplicate router imports and middleware usage.

--- server.js
+++ server.js
@@ -7,11 +7,10 @@ const PORT = process.env.PORT || 3000
 const authRouter = require('./routes/authRouter')
 const communityRouter = require('./routes/communityRouter')
 const newsletterRouter = require('./routes/newsletterRouter')
-const db = require('./db')
-const authRouter = require('./routes/authRouter')
 const eventRouter = require('./routes/eventRouter')
 
-const communityRouter = require('./routes/communityRouter')
+
+
 const db = require('./db')
 const app = express()
 
@@ -23,9 +22,8 @@ app.use(express.urlencoded({ extended: false }))
 app.use('/auth', authRouter)
 app.use('/communities', communityRouter)
 app.use('/newsletter', newsletterRouter)
-app.use('/auth', authRouter)
 app.use('/event', eventRouter)
-app.use('/communities', communityRouter)
+
 
 app.get('/', (req, res) => {
   res.send("Server's Running!")
